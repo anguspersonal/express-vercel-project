59346e731fd2b7f401066b4030513f269a407ce9
Angus Hally Anmut <angus.hally@gmail.com>
2024-07-14 13:03:51 +0100

    message for laura

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send(`Hello Laura! I'm a website built with Express on Vercel`));
+app.get('/', (req, res) => res.send(`Laura I love you so much, I built a website so that everyone in the world would know.`));
 
 const port = process.env.PORT || 3000;
 
