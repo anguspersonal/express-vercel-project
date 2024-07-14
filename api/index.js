53755daf896d49e4b4177bf38f089951385a3d21
Angus Hally Anmut <angus.hally@gmail.com>
2024-07-14 12:37:50 +0100

    changed response message to remove d in front of vercel

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send(`Hello Laura! I'm a website built with Express on dVercel`));
+app.get('/', (req, res) => res.send(`Hello Laura! I'm a website built with Express on Vercel`));
 
 const port = process.env.PORT || 3000;
 
