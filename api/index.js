bd633d74e5fe34e9ad5af6eccf74f93f4e1968bb
Angus Hally Anmut <angus.hally@gmail.com>
2024-07-14 12:13:57 +0100

    Description of the changes you made

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Express on dVercel'));
+app.get('/', (req, res) => res.send(`Hello Laura! I'm a website built with Express on dVercel`));
 
 app.listen(3000, () => console.log('Server ready on port 3000.'));
 
